d112e5ae5e96405dd0c91cbb417e83f13ea8057b
bhuvana-sridhara <bsridh5@uic.edu>
2021-04-09 13:10:17 -0500

    adding build.gradle and curldemo.java

--- CurlGen/src/CurlDemo.java
+++ CurlGen/src/CurlDemo.java
@@ -1,2 +1,28 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class CurlDemo {
+
+    public void userCode(){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://www.google.com"))
+                .header("test", "hell yeah")
+                .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
+
+        HttpRequestCurlCommandBuilder test = new HttpRequestCurlCommandBuilder();
+    }
+
+    public static void main(String[] args) {
+        CurlDemo demo = new CurlDemo();
+        demo.userCode();
+        System.out.println("done");
+    }
+
 }
