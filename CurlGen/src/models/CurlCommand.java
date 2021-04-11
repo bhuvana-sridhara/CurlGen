249ac1de9c85a8686afd5bb8d529fe60e02bf72b
Prajwal K Kammardi <prajwalkkammardi@gmail.com>
2021-04-11 08:51:37 -0500

    Removed unneeded classes

--- CurlGen/src/models/CurlCommand.java
+++ CurlGen/src/models/CurlCommand.java
@@ -7,7 +7,12 @@ import java.util.Map;
 
 public class CurlCommand {
 
-    private CurlOptions curlOptions;
+    private static final Map<String, String> curlOptions = Map.of(
+            "--location", "",
+            "--request", "27",
+            "--header", "10",
+            "--data-raw", "51"
+    );
     private final List<String> options = new ArrayList<String>();
     private final Map<String, String> headers = new LinkedHashMap<String, String>();
     private final List<String> locations = new ArrayList<String>(4);
@@ -19,24 +24,24 @@ public class CurlCommand {
 
         return "";
     }
-
-    public CurlOptions getCurlOptions() {
-        return curlOptions;
-    }
-
-    public void setCurlOptions(CurlOptions curlOptions) {
-        this.curlOptions = curlOptions;
-    }
-
+    
     public List<String> getOptions() {
+
         return options;
     }
 
     public Map<String, String> getHeaders() {
+        //TODO
         return headers;
     }
 
     public List<String> getLocations() {
+        // TODO
         return locations;
+    }
+
+    public String generateCurlString() {
+        // TODO
+        return "";
     }
 }
