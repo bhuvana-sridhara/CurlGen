10a6a325d795b758f21b4ba4bbab27e4f463c660
Prajwal K Kammardi <prajwalkkammardi@gmail.com>
2021-04-11 10:14:57 -0500

    Added URL check and custom exceptions

--- CurlGen/src/CurlCommandBuilder.java
+++ CurlGen/src/CurlCommandBuilder.java
@@ -1,10 +1,56 @@
+import exceptions.InvalidUrlFormatException;
 import models.CurlCommand;
 
-public class CurlCommandBuilder implements ICurlCommandBuilder{
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CurlCommandBuilder implements ICurlCommandBuilder {
 
     private CurlCommand curlCommand;
+
     @Override
     public CurlCommand GenerateCommand() {
         return null;
     }
+
+    public void buildHeaders() {
+        // TODO --header which is a MAP<String, String> and convert to JSON format
+    }
+
+    public void buildOptions() {
+        //TODO --request GET POST PUT DELETE UPDATE
+
+        // TODO --data-raw
+    }
+
+    public void buildUrl(String url) {
+        // TODO add url
+        try {
+            isValidUrl(url);
+        } catch (InvalidUrlFormatException e) {
+            System.err.println(e);
+            System.exit(-1);
+        }
+
+
+    }
+
+    private static boolean isValidUrl(String url) throws InvalidUrlFormatException{
+        String regex = "((http|https)://)(www.)?"
+                + "[a-zA-Z0-9@:%._\\+~#?&//=]"
+                + "{2,256}\\.[a-z]"
+                + "{2,6}\\b([-a-zA-Z0-9@:%"
+                + "._\\+~#?&//=]*)";
+
+        Pattern pattern = Pattern.compile(regex);
+        if(url == null){
+            throw new InvalidUrlFormatException("No URL Found. Please check the URL and run again");
+        }
+        Matcher matcher = pattern.matcher(url);
+        if (!matcher.matches())
+            throw new InvalidUrlFormatException("Bad URL. Please check the URL and run again. URL: "+ url);
+        else
+            return true;
+    }
+
 }
---
+++ CurlGen/src/exceptions/InvalidUrlFormatException.java
@@ -0,0 +1,14 @@
+package exceptions;
+/*
+ *
+ * Created by: prajw
+ * Date: 11-Apr-21
+ *
+ */
+
+public class InvalidUrlFormatException extends Exception{
+
+    public InvalidUrlFormatException(String message) {
+        super(message);
+    }
+}
