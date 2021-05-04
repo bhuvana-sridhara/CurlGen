78d7470f2bb7eaf2b09f11cd4453ce6cf1530846
Prajwal K Kammardi <prajwalkkammardi@gmail.com>
2021-05-04 15:45:15 -0500

    Add OkHttp

--- src/main/java/com/cs540/curlgen/models/Option.java
+++ src/main/java/com/cs540/curlgen/models/Option.java
@@ -1,9 +1,26 @@
 package com.cs540.curlgen.models;
 
 public enum Option {
-    HEADER("-H"),
+    AGENT("-A"),
+    COOKIENAME("-b"),
+    COOKIEJAR("-c"),
+    COMPRESSED("--compressed"),
     TIMEOUT("--connect-timeout"),
-    METHOD("-X");
+    DATA("-d"),
+    DATABINARY("--data-binary"),
+    FORM("-F"),
+    HEADER("-H"),
+    INSECURE("-k"),
+    MAXTIME("-m"),
+    METHOD("-X"),
+    OUTPUT("-o"),
+    REMOTENAME("-O"),
+    HEADERONLY("--head"),
+    USERPWD("-u");
+
+
+
+
 
     public final String value;
 
